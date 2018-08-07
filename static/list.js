1be680b39e4ff33733ff5be81eb5e2d1dc416403
diartyz <diartyz@gmail.com>
2018-08-07 20:03:41 +0800

    feat: add html-minifier

--- static/list.js
+++ static/list.js
@@ -1,12 +1,10 @@
 const options = {
   page: 9,
-  pagination: {
-    outerWindow: 1,
-  },
+  pagination: { outerWindow: 1 },
   valueNames: ['title', { name: 'link', attr: 'href' }],
   item: `
     <h3>
-      <a class="title link"></a>
+      <a class="title link" target="_blank"></a>
     </h3>
   `.trim(),
 }
