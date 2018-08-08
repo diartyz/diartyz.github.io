ba9f76ab3f2070ff7c62771084dd09cc11dc4467
diartyz <diartyz@gmail.com>
2018-08-08 16:09:29 +0800

    feat: add date & tag

--- dist/articles.js
+++ dist/articles.js
@@ -1,1 +1,1 @@
-const articles = [{"name":"test","tag":"test","date":1533641453581,"link":"test/test.html"}]
+const articles = [{"title":"test","tag":"test","date":1533641453581,"link":"test/test.html"}]
---
+++ static/main.js
@@ -0,0 +1,19 @@
+const options = {
+  page: 9,
+  pagination: { outerWindow: 1 },
+  valueNames: ['title', 'date', 'tag', { name: 'link', attr: 'href' }],
+  item: `
+    <h3>
+      <a class="title link" target="_blank"></a>
+      <span class="date"></span>
+      <span class="tag"></span>
+    </h3>
+  `.trim(),
+}
+
+articles.forEach(article => {
+  article.link = 'dist/' + article.link
+  article.date = window.dateFns.format(article.date, 'MMMM DD,YYYY')
+})
+
+const blogList = new List('blog-list', options, articles)
