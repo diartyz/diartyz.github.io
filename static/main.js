ca0195db4301458c4a66113a326ea2f6e9a82bd8
diartyz <diartyz@gmail.com>
2018-08-15 16:21:39 +0800

    feat: update font-size

--- static/main.js
+++ static/main.js
@@ -1,13 +1,13 @@
 const options = {
-  page: 9,
+  page: 12,
   pagination: { outerWindow: 1 },
   valueNames: ['title', 'date', 'tag', { name: 'link', attr: 'href' }],
   item: `
-    <h3>
+    <h4>
       <a class="title link" target="_blank"></a>
       <span class="date"></span>
       <span class="tag"></span>
-    </h3>
+    </h4>
   `.trim(),
 }
 
